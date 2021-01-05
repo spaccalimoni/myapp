6e14dac4825fdfe8a2d92c8fd0020a693dfa8066
Jacopo <spaccalimoni@gmail.com>
2021-01-05 18:20:02 +0100

    rtsp-relay #1st try

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+const { proxy } = require('rtsp-relay')(app);
+const handler = proxy({url:'rtsp://192.168.1.61:7447/OluifM93AT7FMb7I',verbose: false,});
+
+app.ws('/api/stream', handler);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express.js' });
